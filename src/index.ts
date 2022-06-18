8abc724565a2e9894a421a51c1ec41ac6f43ff45
Alexander Suslov <sasha_suslov07@mail.ru>
2022-06-19 01:38:29 +0300

    refactor: move out router from index.ts to separate module

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,7 @@
 import { createServer } from 'http';
-import {
-  getUsers, getUser, createUser, updateUser, deleteUser,
-} from './controllers/userController';
-import { ID_PARAM_ORDER } from './const/minor';
-import {
-  isDeleteUserRequest, isGetUserRequest, isGetUsersRequest,
-  isInvalidMethod,
-  isPostUserRequest, isPutUserRequest, isUserGetOrPutOrDeleteRequest,
-} from './utils/requestUtils';
-import { replyBadRequest, replyNotFound } from './utils/responseUtils';
-import isIdValid from './utils/isIdValid';
+import router from './router/router';
 
-const server = createServer();
+const DEFAULT_PORT = 5000;
+const PORT = process.env.PORT || DEFAULT_PORT;
 
-server.on('request', (req, res) => {
-  if (isGetUsersRequest(req)) {
-    getUsers(req, res);
-  } else if (isPostUserRequest(req)) {
-    createUser(req, res);
-  } else if (isUserGetOrPutOrDeleteRequest(req)) {
-    const id = req.url?.split('/')[ID_PARAM_ORDER];
-
-    if (!isIdValid(id)) {
-      replyBadRequest(res);
-    } else if (isGetUserRequest(req)) {
-      getUser(req, res, id);
-    } else if (isPutUserRequest(req)) {
-      updateUser(req, res, id);
-    } else if (isDeleteUserRequest(req)) {
-      deleteUser(req, res, id);
-    }
-  } else if (isInvalidMethod(req)) {
-    replyBadRequest(res);
-  } else {
-    replyNotFound(res);
-  }
-});
-
-// TODO add .env file
-const PORT = process.env.PORT || 5000;
-
-server.listen(PORT, () => console.log('Server running on port 5000'));
+createServer(router).listen(PORT, () => process.stdout.write(`Server running on port ${PORT}\n`));
